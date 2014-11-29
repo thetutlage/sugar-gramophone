b7ab8398cda6f9910a5bf75629f8ba4a8f38bab5
Harminder Virk <virk.officials@gmail.com>
2014-11-29 10:28:39 +0530

    Fixed core lib path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('sugar-angularjs').module('gramophone.core',[]);
 var gramophone = require('./src/notify.core.min.js');
-window.gramophone = gramophone;
+window.gramophone = gramophone.amplify;
 app.service('gramophone',function() {
   return window.gramophone;
 });
+
